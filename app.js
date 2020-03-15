82b8f926229b91549c8042ebf9c3d4e028ee584d
Raiah Mohamed Amine <hm_raiah@esi.dz>
2020-03-15 11:35:38 +0100

    DEV : Uploading a form with formidable

--- app.js
+++ app.js
@@ -10,14 +10,15 @@ var handleBars = require ('express3-handlebars').create ({
         }
     }
 });
+var formidale = require('formidable');
 var fortunes = require('./fortune'); 
 var weather = require('./weather');
 
 app.set('port', process.env.PORT || 3000) ;
-
 app.use (express.static(__dirname + '/public'));
 app.engine('handlebars', handleBars.engine) ;
 app.set ('view engine', 'handlebars');
+app.use(require('body-parser') ());
 
 app.use ((req,res,next)=> {
     res.locals.showTests = app.get('env')!== 'production' && req.query.test==='1' ;
@@ -52,20 +53,35 @@ app.get('/download', (req,res)=> {
     res.download (__dirname +'/public/img/Logo.jpg', (err)=> {
         console.log (err);
     });
-    res.send('download') ;
-})
+});
+
+app.get ('/contest/vacation-photo', (req,res)=> {
+    var now = new Date ();
+    res.render ('contest/vacation-photo', {
+        year : now.getFullYear(),
+        month : now.getMonth ()
+    });
+
+});
+
+app.post ('/contest/vacation-photo/:year/:month', (req,res)=>{
+    var form = new formidale.IncomingForm ();
+    form.parse (req, (err,fields,files)=> {
+        if (err) res.status(404).render ('404');
+        console.log (fields);
+        console.log (files);
+    })
+});
 
 app.use ((req,res)=> {
     res.render('404');
 });
 
-
 app.use ((req,res)=> {
     res.type('text/plain');
     res.status (500);
     res.send('500 - Not Found');
-})
-
+});
 
 app.listen (app.get ('port'), ()=> {
     console.log ('Server started at port : ' + app.get('port'));
